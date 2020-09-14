d2825f4d36053dc4dc13c4e2a7dfb9bc5e3a6f30
pally-ds <zulfadhli.zaki@beicip.asia>
2020-09-15 06:21:40 +0800

    Add reset button functionality

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,22 +1,30 @@
-// Create a function which takes the number of square grid.
-// Create the div box within the container similar to the number of square grid
+function createSquareDiv(squareSize) {
+    let size = 960/squareSize;
 
-function createSquareDiv() {
-    const div = document.createElement('div');
-    let size = 960/squarePerSide;
-
-    for (let i=0; i<squarePerSide; i++ ) {   
-        for (let j=0; j<squarePerSide; j++ ) {
-            const div = document.createElement('div');
+    for (let i=0; i<squareSize; i++ ) {   
+        for (let j=0; j<squareSize; j++ ) {
+            let div = document.createElement('div');
             div.style.width = size + "px";
-            div.style.height = size + "px";
+            div.style.height = size  + "px";
             div.classList.toggle('squareBlock');
 
             container.appendChild(div);
+
+            div.addEventListener('mouseover', (e) =>{
+                e.target.style.background = "black";    
+            });
         }
     }
 }
 
+function resetSketchBoard(){
+    let squareSize = prompt('Please enter the number of grids', '64');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    createSquareDiv(squareSize);
+}
 
 
 // Initialize inital parameter
@@ -28,18 +36,22 @@ container.style.width = "960px";
 container.style.height = "960px";
 container.style.border = "5px solid black";
 
-createSquareDiv();
+createSquareDiv(squarePerSide);
 
 // Add event listener on hover to the div element
 
-const divs = document.querySelectorAll('.squareBlock')
-divs.forEach((div) =>{
+//let divs = document.querySelectorAll('.squareBlock')
+//divs.forEach((div) =>{
     
-    div.addEventListener('mouseover', (e) =>{
-        e.target.style.background = "black";
-    
-    });
-})
+//    div.addEventListener('mouseover', (e) =>{
+//        e.target.style.background = "black";    
+//    });
+//})
+
+const btn = document.querySelector('#resetBtn');
+btn.addEventListener('click', resetSketchBoard);
+
+
 
 
 
